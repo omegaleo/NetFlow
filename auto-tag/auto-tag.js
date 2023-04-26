55704f8693df174b27156455c10dd7318e419d8f
OmegaLeo <nunodiogo@omegaleo.pt>
2023-04-26 20:56:59 +0100

    Updates to auto-tag

---
+++ auto-tag/auto-tag.js
@@ -0,0 +1,33 @@
+const { context, getOctokit } = require('@actions/github');
+const octokit = getOctokit(process.env.UNITYFLOW_SECRET);
+const issueNumber = context.payload.issue.number;
+const issueTitle = context.payload.issue.title;
+const issueBody = context.payload.issue.body;
+const isFeature = /@feature/gi.test(issueBody) || /@feature/gi.test(issueTitle);
+const isBug = /@bug/gi.test(issueBody) || /@bug/gi.test(issueTitle);
+const isEnhancement = /@enhancement/gi.test(issueBody) || /@enhancement/gi.test(issueTitle);
+
+if (isFeature) {
+  octokit.issues.addLabels({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issueNumber,
+    labels: ['feature']
+  });
+}
+if (isBug) {
+  octokit.issues.addLabels({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issueNumber,
+    labels: ['bug']
+  });
+}
+if (isEnhancement) {
+  octokit.issues.addLabels({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issueNumber,
+    labels: ['enhancement']
+  });
+}
