37dd23440cdfc562ed11c8e9e6aa2305b71b39bc
OmegaLeo <nunodiogo@omegaleo.pt>
2023-04-26 21:28:15 +0100

    Changed auto-tag to c#

---
+++ auto-tag/UnityFlow.AutoTag/Program.cs
@@ -0,0 +1,28 @@
+﻿using Octokit;
+
+class Program {
+    static async Task Main(string[] args) {
+        var client = new GitHubClient(new ProductHeaderValue("unityflow"));
+        var tokenAuth = new Credentials(Environment.GetEnvironmentVariable("UNITYFLOW_SECRET"));
+        client.Credentials = tokenAuth;
+
+        var issueNumber = int.Parse(args[0]);
+        var issue = await client.Issue.Get("OWNER", "REPO", issueNumber);
+        var issueUpdate = new IssueUpdate();
+
+        if (issue.Title.Contains("@issue") || issue.Body.Contains("@issue")) {
+            issueUpdate.AddLabel("issue");
+        }
+
+        if (issue.Title.Contains("@feature") || issue.Body.Contains("@feature")) {
+            issueUpdate.AddLabel("feature");
+        }
+
+        if (issue.Title.Contains("@enhancement") || issue.Body.Contains("@enhancement")) {
+            issueUpdate.AddLabel("enhancement");
+        }
+
+        var updatedIssue = await client.Issue.Update("OWNER", "REPO", issueNumber, issueUpdate);
+        Console.WriteLine(updatedIssue.Number);
+    }
+}
