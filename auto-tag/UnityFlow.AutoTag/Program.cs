a26407338a6454a8871befed303b0dda1ea9aeb1
OmegaLeo <nunodiogo@omegaleo.pt>
2023-04-30 12:33:44 +0100

    Now generating documentation for auto-tag

--- auto-tag/UnityFlow.AutoTag/Program.cs
+++ auto-tag/UnityFlow.AutoTag/Program.cs
@@ -2,10 +2,14 @@
 using Octokit;
 using UnityFlow.AutoTag.Models;
 using UnityFlow.DocumentationHelper.Library.Documentation;
+using UnityFlow.DocumentationHelper.Library.Helpers;
 
 class Program {
     [Documentation("Main", "This component is in charge of auto-tagging issues by using a filter that's loaded from filters.json and is case insensitive.", new string[]{})]
-    static async Task Main(string[] args) {
+    static async Task Main(string[] args)
+    {
+        GenerateDocumentation();
+        
         var client = new GitHubClient(new ProductHeaderValue("unityflow"));
         var tokenAuth = new Credentials(Environment.GetEnvironmentVariable("UNITYFLOW_SECRET"));
         client.Credentials = tokenAuth;
@@ -28,5 +32,37 @@ class Program {
 
         var updatedIssue = await client.Issue.Update(owner, repo, issueNumber, issueUpdate);
         Console.WriteLine($"Successfully tagged Issue #{updatedIssue.Number}");
+    }
+
+    public static async Task GenerateDocumentation()
+    {
+        var docs = DocumentationHelperTool.GenerateDocumentation();
+        
+        var client = new GitHubClient(new ProductHeaderValue("unityflow"));
+        var tokenAuth = new Credentials(Environment.GetEnvironmentVariable("UNITYFLOW_SECRET"));
+        client.Credentials = tokenAuth;
+
+        var filePath = "auto-tag-documentation.md";
+        
+        if (!File.Exists(filePath))
+        {
+            await File.WriteAllTextAsync(filePath, "");
+        }
+
+        var documentation = "";
+
+        foreach (var doc in docs)
+        {
+            documentation += $"# {doc.AssemblyName}{Environment.NewLine}  ";
+            documentation += $"## {doc.ClassName}{Environment.NewLine}  ";
+
+            foreach (var desc in doc.Descriptions)
+            {
+                documentation += $"### {desc.Title}{Environment.NewLine}  ";
+                documentation += $"{desc.Description}{Environment.NewLine}  ";
+            }
+        }
+        
+        File.WriteAllText(filePath, documentation);
     }
 }
